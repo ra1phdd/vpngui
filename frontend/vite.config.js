b5e34a2f6db1da82a499d8a167e1ef8df82d23b5
ra1ph <kneshkreba@mail.ru>
2024-11-13 17:20:27 +0300

    fix/minor-bugs

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,6 +4,11 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      external: [],
+    },
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
