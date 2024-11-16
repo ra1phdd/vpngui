ab8a4aa40eb3622b3a8074a9069b012037eb4d84
ra1ph <kneshkreba@mail.ru>
2024-11-16 13:42:37 +0300

    fix/error-isgooswindows

--- frontend/src/components/common/windowControls.jsx
+++ frontend/src/components/common/windowControls.jsx
@@ -2,22 +2,13 @@ import '../../assets/styles/components/windowControls.css'
 import {useEffect, useState} from "react";
 import * as App from "../../../wailsjs/go/app/App.js";
 import {WindowHide, WindowMinimise} from "../../../wailsjs/runtime/runtime.js";
-import {toast} from "react-toastify";
 
 function WindowControls() {
     const [isWindows, setIsWindows] = useState(false);
 
     useEffect(async () => {
-
-        try {
-            const result = await App.IsGOOSWindows();
-            if (result !== null) {
-                throw new Error(result);
-            }
-            setIsWindows(result);
-        } catch (error) {
-            toast.error(`Ошибка: ${error}`);
-        }
+        const result = await App.IsGOOSWindows();
+        setIsWindows(result);
     }, []);
 
     const closeWindow = () => {
